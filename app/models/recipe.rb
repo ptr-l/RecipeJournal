8bfb02d9817bb12bfab50aeae7fd91ab78310f44
Peter Lamson <ptrlmsn@gmail.com>
2022-08-09 16:08:09 -0400

    fixed image thumbnails linking to themselves, rather than actual image

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,9 +2,8 @@ class Recipe < ApplicationRecord
     belongs_to :user 
     has_many_attached :images
 
-    def image_thumbnails 
-        images.map do |image| 
-            image.variant(resize_to_limit: [250, 250]).processed 
-        end 
+    def image_thumbnail (im)
+        im.variant(resize_to_limit: [250, 250]).processed 
     end 
+        
 end
